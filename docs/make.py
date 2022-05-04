734234088921bb06874f00da4082a90d644b3684
Dmitry Kozerovsky <dima29koz@yandex.ru>
2022-05-04 17:56:54 +0300

    Update docs

--- docs/make.py
+++ docs/make.py
@@ -1,7 +1,7 @@
 r"""
-C:\Users\dima2\OneDrive\Документы\учеба\PythonProjects\Maze_game\venv\Scripts\sphinx-apidoc -o ./source ..
+sphinx-apidoc -o ./source ..
 """
 
 r"""
-C:\Users\dima2\OneDrive\Документы\учеба\PythonProjects\Maze_game\venv\Scripts\sphinx-build -b html ./source/ ./build/html
+sphinx-build -b html ./source/ ./build/html
 """
